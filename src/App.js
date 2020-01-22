2b6b876ea587fd1620dae1dde2ca06c6e3560abd
trippi hippi <reet.p14@gmail.com>
2020-01-23 04:06:53 +0530

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import codeicon from "./code.png";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+
+const useStyle = makeStyles(theme => ({
+  app: {
+    backgroundColor: "#6498ff",
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  appbar: {
+    height: "64px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  intro: {
+    fontSize: "2rem",
+    fontFamily: "cursive",
+    color: "white"
+  }
+}));
 
 function App() {
+  const classes = useStyle();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app}>
+      <AppBar className={classes.appbar}>
+        <Typography className={classes.intro}>Welcome</Typography>
+      </AppBar>
+      <Typography className={classes.intro}>Hello, I'm Reet!</Typography>
     </div>
   );
 }
